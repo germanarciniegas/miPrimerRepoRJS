f181a1488f8693358a18580dae0c7729411c6ec8
GERMAN ARCINIEGAS <germanarciniegas@MacBook-Air-de-GERMAN.local>
2023-12-13 21:18:34 -0500

    Life cycle react

--- german-proyecto/src/App.jsx
+++ german-proyecto/src/App.jsx
@@ -2,31 +2,88 @@ import './App.css';
 import React from 'react';
 
 function Padre() {
-  const[frutas, setFrutas] = React.useState(['manzana','banana']);
+  const[digimones, setDigimons] = React.useState([]);
   
-  const agragarPera = (ref) => {
-    console.log(ref.current.value);
-    setFrutas((frutasAnteriores) => [...frutasAnteriores,ref.current.value])
-  };
+  React.useEffect(()=> {
+      fetch('https://digimon-api.vercel.app/api/digimon')
+      .then((response) => response.json())
+      .then((dataDigimon)=> {
+        setDigimons(dataDigimon)
+      })
+  },[])
+
+  console.log(digimones)
+
+
+  // React.useEffect(()=> {
+  //   //en cada render
+  // })
+
+  // React.useEffect(()=> {
+  //   //componentDidMount => 1
+  //   const display = document.getElementById('');
+  //   ///fetch API de google USUARIO
+  // },[])
+
+  // React.useEffect(()=> {
+  //   //componentDidUpdate => infinitas
+
+  // },[frutas, props.german])
+
+  // React.useEffect(()=> {
+  //   //componentWillUnmount() => 1
+  //   const evenetListener = 
+  //   document.getElementById('body').addEventListener('scroll',()=>{
+  //     //mostrar algo importante
+  //   });
+
+  //   return () => document.getElementById('body').removeEventListener(evenetListener)
+  // },[])
+  
 
   return (
     <div className="App">
-      <Hijo agragado={agragarPera} mercado={frutas}/>
+      {digimones.map((digimon)=>{
+          return <DigimonCard name={digimon.name} img={digimon.img} level={digimon.level}/>
+        })
+      }
     </div>
   );
 }
 
 export default Padre;
 
-function Hijo(props) {
-  const myRef = React.useRef(null);
+const DigimonCard = (props) => {
+
   return (
-    <div>
-      <h1>Mi Primer Proyecto Publico</h1>
-      <input ref={myRef}/>
-      {props.mercado.map((fruta)=><h2>{fruta}</h2>)}
-      {/* <Button miCallback={() => miCallback()}/> */}
-      <button onClick={()=>props.agragado(myRef)}>Agregar Fruta</button>
+    <div className='container-card'>
+     <div className='title'>
+      <h1>{props.name}</h1>
+     </div>
+     <div className='imagen'>
+        <img src={props.img} alt=""/>
+     </div>
+     <div className='level'>
+      <p>{props.level}</p>
+     </div>
     </div>
   );
 }
+
+// class MyComponent extent React {
+//   constructor(){
+
+//   }
+//   componentDidMount() {
+//     //
+//   }
+//   componentDidUpdate() {
+//     //
+//   }
+//   componentWillUnmount() {
+//     //
+//   }
+//   render(
+//     <div></div>
+//   )
+// } 
